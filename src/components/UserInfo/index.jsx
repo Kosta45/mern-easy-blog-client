76613531a4e5b124ca8a1ad3327969a610418f1d
Kostiantyn <konstanlebed8@gmail.com>
2026-07-11 14:03:27 +0200

    fix: update UserInfo component to use separate firstName and lastName props

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -1,16 +1,25 @@
 import styles from "./UserInfo.module.scss";
 import PersonIcon from "@mui/icons-material/Person";
 
-export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
+export const UserInfo = ({
+  avatarUrl,
+  firstName,
+  lastName,
+  additionalText,
+}) => {
   return (
     <div className={styles.root}>
       {avatarUrl ? (
-        <img className={styles.avatar} src={avatarUrl} alt={fullName} />
+        <img
+          className={styles.avatar}
+          src={avatarUrl}
+          alt={`${firstName} ${lastName}`}
+        />
       ) : (
         <PersonIcon />
       )}
       <div className={styles.userDetails}>
-        <span className={styles.userName}>{fullName}</span>
+        <span className={styles.userName}>{`${firstName} ${lastName}`}</span>
         <span className={styles.additional}>{additionalText}</span>
       </div>
     </div>
